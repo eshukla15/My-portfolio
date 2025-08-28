24ff881f29da79edba955f2dc0aa54e9af4ee225
eshukla15 <eshukla15@gmail.com>
2025-08-28 20:41:44 +0530

    dark-light theme

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen  bg-background text-foreground overflow-x-hidden">Hello
+            {/*theme toggle*/}
+            <ThemeToggle />
+
+            {/*bg effects*/}
+
+            {/*navbar*/}
+
+            {/*main content*/}
+
+            {/*Footer*/}
+
+        </div>
+    )
+};
