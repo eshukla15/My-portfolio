020c6a6182d4f006efcae01a0ea69d608cd3fa76
eshukla15 <eshukla15@gmail.com>
2025-08-30 03:22:16 +0530

    Completed

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -11,7 +11,8 @@ export const AboutSection = () => {
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-          <div className="space-y-6">
+          <div className="flex flex-col items-center space-y-6 ">
+            
             <h3 className="text-2xl font-semibold">
               Aspiring Ethical Hacker
             </h3>
@@ -31,10 +32,10 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1VBDo-HHzVsLUDBXcucTOT7b4lNG7drz1/view?usp=sharing"
                 className="px-6  py-2 font-bold rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
-                Download CV
+                Download Resume
               </a>
             </div>
           </div>
